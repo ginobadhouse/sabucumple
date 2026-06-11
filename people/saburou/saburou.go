1f670717de885bf8cb66972c56770114886232ab
平城三郎 <heizeisaburou@gmail.com>
2026-06-11 15:02:21 +0200

    bug fixed

--- people/saburou/saburou.go
+++ people/saburou/saburou.go
@@ -24,20 +24,22 @@ var tmpl = template.Must(template.New("index").Parse(indexTemplate))
 
 // Person representa a alguien a quien agradecer en la página.
 type Person struct {
-	Name    string // Nombre visible, no tiene por qué coincidir con el nombre de la imagen.
-	Image   string // Fichero dentro de assets/.
-	Message string
-	URL     string // Opcional.
-	Active  bool   // Si aún no tiene carpeta/módulo propio, se deja desactivado.
+	Name     string // Nombre visible, no tiene por qué coincidir con el nombre de la imagen.
+	Image    string // Fichero dentro de assets/.
+	Message  string
+	URL      string // Opcional.
+	Endpoint string // Opcional, ruta a su propia página dentro del sitio (ej. "/chavsi").
+	Active   bool   // Si aún no tiene carpeta/módulo propio, se deja desactivado.
 }
 
 var thanks = []Person{
 	{
-		Name:    "Chavsi",
-		Image:   "chavsi.png",
-		Message: "Gracias por apoyar la idea del proyecto común y aportar tus ideas! Siento mucho haberte sacado de Flask! :3",
-		URL:     "https://github.com/Chavsi",
-		Active:  true,
+		Name:     "Chavsi",
+		Image:    "chavsi.png",
+		Message:  "Gracias por apoyar la idea del proyecto común y aportar tus ideas! Siento mucho haberte sacado de Flask! :3",
+		URL:      "https://github.com/Chavsi",
+		Endpoint: "/chavsi/",
+		Active:   true,
 	},
 	{
 		Name:    "Dixion",
@@ -60,25 +62,28 @@ var thanks = []Person{
 		Active: true,
 	},
 	{
-		Name:   "Leandro",
-		Image:  "leandro.png",
-		Message: "Gracias por apoyarme en cada uno de los problemas que he tenido y que te he contado y por dejarme usar MidOS de vez en cuando. ¡Eres una gran persona!",
-		URL:    "https://midos.dev/",
-		Active: true,
+		Name:     "Leandro",
+		Image:    "leandro.png",
+		Message:  "Gracias por apoyarme en cada uno de los problemas que he tenido y que te he contado y por dejarme usar MidOS de vez en cuando. ¡Eres una gran persona!",
+		URL:      "https://midos.dev/",
+		Endpoint: "/midos/",
+		Active:   true,
 	},
 	{
-		Name:    "Lein",
-		Image:   "lein.png",
-		Message: "Gracias por crear una página tan guay para mi con tantos efectos especiales y por ayudarme a mi y a los demás con tantos problemas que hemos tenido.",
-		URL:     "https://github.com/CalumRakk",
-		Active:  true,
+		Name:     "Lein",
+		Image:    "lein.png",
+		Message:  "Gracias por crear una página tan guay para mi con tantos efectos especiales y por ayudarme a mi y a los demás con tantos problemas que hemos tenido.",
+		URL:      "https://github.com/CalumRakk",
+		Endpoint: "/leinSeab/",
+		Active:   true,
 	},
 	{
-		Name:    "Savage",
-		Image:   "savage.png",
-		Message: "Gracias a evitar que pierda el timepo convenciendome de no hacer implementaciones imposibles en go.",
-		URL:     "https://github.com/lsproule",
-		Active:  true,
+		Name:     "Savage",
+		Image:    "savage.png",
+		Message:  "Gracias por la tarta y por evitar que pierda el timepo convenciendome de no hacer implementaciones imposibles en go.",
+		URL:      "https://github.com/lsproule",
+		Endpoint: "/savage/",
+		Active:   true,
 	},
 }
 
